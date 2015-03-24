c1cdf01318d81071b0d53402595469b326f2da3c
Seth Johnson <sethjdesigns@gmail.com>
2015-03-24 15:10:02 -0400

    Afternoon String Manipulation Lab
    Not optimized

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -30,6 +30,36 @@ namespace StringManipulation
             //Console.ReadLine();
 
 
+
+
+
+
+            /*
+            3.24 Afternoon String Lab
+            */
+
+           //Instantiate the string as an array of strings
+           string[] stringArray = new string[] { "This", "is", "a", "string", "of", "text" };
+
+           //Concat each array index as a single string in reverse order
+           string stringArrayReverse = (stringArray[5] + " " + stringArray[4] + " " + stringArray[3] + " " + stringArray[2] + " " + stringArray[1] + " " + stringArray[0]);
+
+           //Print stringArrayReverse to the console
+           Console.WriteLine(stringArrayReverse);
+
+           //Concat each array index as a single string in normal order and transform text to uppercase.
+           string stringArrayUpperCase = (stringArray[0] + " " + stringArray[1] + " " + stringArray[2] + " " + stringArray[3] + " " + stringArray[4] + " " + stringArray[5]).ToUpper();
+
+           //Print stringArrayUpperCase to the console
+           Console.WriteLine(stringArrayUpperCase);
+
+           //Concat every string as a single string seperated by commas
+           string stringArrayCommas = (stringArray[0] + "," + stringArray[1] + "," + stringArray[2] + "," + stringArray[3] + "," + stringArray[4] + "," + stringArray[5]);
+
+           Console.WriteLine(stringArrayCommas);
+
+           Console.ReadLine();
+
         }
     }
 }
